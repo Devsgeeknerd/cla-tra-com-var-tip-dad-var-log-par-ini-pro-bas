5793f9c4d96c0f955ed4766e94179be0375abe52
Paulo <vanderley.1109@gmail.com>
2024-01-19 16:12:22 -0300

    adicionando conteudo

---
+++ main.ts
@@ -0,0 +1,37 @@
+// String
+// let nome = "Paulo";
+// console.log(nome);
+// nome = "Vanderley";
+
+// Number
+// let idade = 21;
+
+// Boolean
+// let programando = true; // false
+
+// Number
+// let altura = 1.70;
+
+// console.log(idade);
+// console.log(programando);
+// console.log(altura);
+
+// Tipo Explícito
+let nome: string = "Paulo";
+let idade: number = 21;
+let altura: number = 1.75;
+let programando: boolean = true;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof altura);
+console.log(typeof programando);
+
+// Snake Case
+let idade_do_paulo = 20;
+
+// Camel Case
+let idadeDoPaulo = 25;
+
+// Pascal Case
+let IdadeDoPaulo = 30;
